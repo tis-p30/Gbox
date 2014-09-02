06e70488600e51f99562ab76ebdf000c2c3a01e0
kbsx32 <blacksmithx32@gmail.com>
2014-09-02 22:53:35 +0400

    Added qt project. Created main window. Linked first button start signal.

---
+++ Gbox.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-09-02T21:30:13
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Gbox
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        wndmain.cpp
+
+HEADERS  += wndmain.h
+
+FORMS    += wndmain.ui
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
-using namespace std;
+/* Gravity newton box project.
+ * Authors: tis-p30, kbsx32;
+ * File purpose: Run file.
+ */
 
+
+#include <QApplication>
+#include "wndmain.h"
+
+// Main function
 int main(int argc, char *argv[])
 {
-    cout << "Hello world" << endl;
-    return 0;
+  QApplication a(argc, argv);
+  WndMain w;
+  w.show();
+  
+  return a.exec();
 }
